0112e495dd0fbbcc4de42c071419af421cb8d7c2
Chad Woolley <thewoolleyman@gmail.com>
2010-01-16 19:24:08 -0700

    add test framework

--- test/setup_chef_test.rb
+++ test/setup_chef_test.rb
@@ -1,1 +1,16 @@
 #!/usr/bin/env ruby
+require 'test/unit'
+require 'fileutils'
+
+class SetupChefTest < Test::Unit::TestCase
+  def test_prompts
+    assert_equal 'foo', sc(['foo']).readlines.first.strip
+  end
+
+  def sc(args)
+    cmd = 'sh setup_chef ' + args.join(' ')
+    FileUtils.cd(File.dirname(__FILE__) + '/..') do
+      return IO.popen(cmd)
+    end
+  end
+end
